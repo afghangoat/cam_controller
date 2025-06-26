c6fd37a25fb2ed0522c60d051d017da5fac21254
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-06-26 10:30:18 +0200

    Initial commit - Mirrored from my site, file: cam_controller/src/olds/shaderClass.hpp

---
+++ cam_controller/src/olds/shaderClass.hpp
@@ -0,0 +1,22 @@
+#ifndef SHADER_CLASS_HPP
+#define SHADER_CLASS_HPP
+
+#include <GL/glew.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cerrno>
+#include <string>
+
+std::string get_file_contents(const char* filename);
+
+class Shader{
+	public:
+		GLuint ID;
+		Shader(const char* vertexFile,const char* fragmentFile);
+		
+		void Activate();
+		void Delete();
+};
+
+#endif
